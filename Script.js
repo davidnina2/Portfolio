da917041f90c0b9c653a8d9ac3eab08b670e47b9
davidnina2 <57233286+davidnina2@users.noreply.github.com>
2024-02-09 14:06:13 -0500

    Version fonctionnel de i18n

---
+++ Script.js
@@ -0,0 +1,62 @@
+
+let fr = {
+    "Header":{
+        "Home":"Acceuil",
+        "Game":"Jeux",
+        "Language":"Anglais"
+
+    }
+}
+let en = {
+    "Header":{
+        "Home":"Home",
+        "Game":"Games",
+        "Language":"French"
+
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+});
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        const chars = key.split('.');
+        let jsonCurrent = langData;
+        let text = "";
+        for(let i = 0;i<chars.length;i++){
+            jsonCurrent = jsonCurrent[chars[i]];
+        }
+        element.textContent = jsonCurrent;
+    });
+}
+function fetchLanguageData(lang) {
+    let response;
+    if(lang == "fr"){
+        //response = jQuery.getJSON("./languages/fr.json");
+        response = fr;
+    } else{
+        //response = jQuery.getJSON("./languages/en.json");
+        response = en;
+    }
+    return response;
+}
+function changeLanguage() {
+    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    let lang;
+    if(userPreferredLanguage == "en"){
+        lang = "fr";
+    } else{
+        lang = "en";
+    }
+    setLanguagePreference(lang);
+    const langData = fetchLanguageData(lang);
+    updateContent(langData);
+}
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+    location.reload();
+}
