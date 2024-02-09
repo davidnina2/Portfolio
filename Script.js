d5a3346496ed5927c414b5bc3a43032d249b78b8
davidnina2 <57233286+davidnina2@users.noreply.github.com>
2024-02-09 15:26:05 -0500

    remove script that cause error

--- Script.js
+++ Script.js
@@ -50,21 +50,3 @@ function setLanguagePreference(lang) {
     location.reload();
 }
 
-//for image zoom
-function zoomImage(position) {
-    const image = document.getElementsByClassName('zoomedImage')[position];
-      image.classList.toggle('zoomed');
-}
-  //pour le sticky header
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
